ed824ed61eac63cc5ac34befdcf9b1f20f992cc6
VLAD F <woohda@ya.ru>
2025-05-08 00:47:48 +0800

    fea function formatRelativeDate in utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,22 @@
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
+import { format, formatDistanceToNowStrict } from 'date-fns'
 
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+export function formatRelativeDate(date: Date) {
+	const currentDate = new Date()
+	if (currentDate.getTime() - date.getTime() < 24 * 60 * 60 * 1000) {
+		return formatDistanceToNowStrict(date, {
+			addSuffix: true
+		})
+	} else {
+		if (currentDate.getFullYear() === date.getFullYear()) {
+			return format(date, 'd MMM')
+		} else {
+			return format(date, 'd MMM, yyyy')
+		}
+	}
 }
