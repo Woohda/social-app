2352a1884de88c0e1bf3c2e17a6b738766d124f9
VLAD F <woohda@ya.ru>
2025-05-07 23:28:44 +0800

    feat validation schema posts and add action create post

---
+++ src/components/posts/editor/actions.ts
@@ -0,0 +1,24 @@
+import { validateRequest } from '@/auth'
+import prisma from '@/lib/prisma'
+import { createPostSchema } from '@/lib/validation'
+import { revalidatePath } from 'next/cache'
+
+export async function submitPost(post: string) {
+	const { user } = await validateRequest()
+	if (!user) {
+		throw new Error('Вы не авторизованы')
+	}
+
+	const { content } = createPostSchema.parse({
+		content: post
+	})
+
+	await prisma.post.create({
+		data: {
+			content,
+			userId: user.id
+		}
+	})
+
+	revalidatePath('')
+}
