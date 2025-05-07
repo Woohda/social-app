7d578f9fd5d9c99570a9a7b9c13d84394a805fbe
VLAD F <woohda@ya.ru>
2025-05-07 12:28:36 +0800

    fix component userButton

--- src/components/UserButton.tsx
+++ src/components/UserButton.tsx
@@ -1,8 +1,19 @@
 'use client'
 
 import useSession from '@/hooks/use-session'
-import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu'
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from './ui/dropdown-menu'
 import UserAvatar from './UserAvatar'
+import Link from 'next/link'
+import { LogOutIcon, UserIcon } from 'lucide-react'
+import { logout } from '@/app/(auth)/actions'
+import { cn } from '@/lib/utils'
 
 interface UserButtonProps {
 	className?: string
@@ -13,10 +24,27 @@ const UserButton = ({ className }: UserButtonProps) => {
 	return (
 		<DropdownMenu>
 			<DropdownMenuTrigger asChild>
-				<button className={className}>
+				<button className={cn('flex-none rounded-full', className)}>
 					<UserAvatar avatarUrl={user?.avatar} size={40} />
 				</button>
 			</DropdownMenuTrigger>
+			<DropdownMenuContent className='w-48'>
+				<DropdownMenuLabel className='flex items-center gap-2'>
+					<UserAvatar avatarUrl={user?.avatar} size={40} /> {user?.username}
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<Link href={`/users/${user?.username}`}>
+					<DropdownMenuItem>
+						<UserIcon className='mr-2 size-4' />
+						Мой профиль
+					</DropdownMenuItem>
+				</Link>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem onClick={() => logout()}>
+					<LogOutIcon className='mr-2 size-4' />
+					Выйти
+				</DropdownMenuItem>
+			</DropdownMenuContent>
 		</DropdownMenu>
 	)
 }
