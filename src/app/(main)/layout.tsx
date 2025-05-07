10058e0d9abcab4ef725e17a5bc451f69971ecd9
VLAD F <woohda@ya.ru>
2025-05-08 00:44:33 +0800

    fix main layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -17,7 +17,7 @@ export default async function Layout({
 		<SessionProvider value={session}>
 			<div className='flex flex-col min-h-screen'>
 				<NavBar />
-				<div className='mx-auto flex w-full grow gap-5 px-5'>
+				<div className='mx-auto flex w-full grow gap-5 p-5'>
 					<MenuBar className='sticky top-[5.25rem] hidden h-fit sm:block flex-none space-y-3 rounded-2xl bg-card py-5 px-3 shadow-sm' />
 					{children}
 				</div>
