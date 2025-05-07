b1dd235ffd0ad36ec6bff1d7b6c21b4272c30c51
VLAD F <woohda@ya.ru>
2025-05-07 14:15:47 +0800

    fix navBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,22 @@
+'use client'
 import SearchField from './SearchField'
 import UserButton from './UserButton'
 import Link from 'next/link'
+import dynamic from 'next/dynamic'
 
 const NavBar = () => {
+	const ThemeButtonNoSSR = dynamic(() => import('./ThemeButton'), {
+		ssr: false
+	})
 	return (
 		<header className='sticky top-0 z-10 bg-card shadow-sm'>
-			<nav className='max-w-7xl mx-auto flex items-center justify-center flex-wrap gap-5 px-5 py-3'>
+			<nav className='max-w-7xl mx-auto flex items-center flex-wrap gap-5 px-5 py-3'>
 				<Link href='/' className='text-2xl font-bold text-primary'>
 					Home
 				</Link>
 				<SearchField />
 				<UserButton className='sm:ms-auto' />
+				<ThemeButtonNoSSR />
 			</nav>
 		</header>
 	)
