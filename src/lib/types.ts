cd4a81835ab10d05c2383fa4dafa520e62c2b8a8
VLAD F <woohda@ya.ru>
2025-05-08 00:45:52 +0800

    feat types PostData satisfies Prisma

---
+++ src/lib/types.ts
@@ -0,0 +1,15 @@
+import { Prisma } from '@prisma/client'
+
+export const postDataInclude = {
+	user: {
+		select: {
+			username: true,
+			name: true,
+			avatarUrl: true
+		}
+	}
+} satisfies Prisma.PostInclude
+
+export type PostData = Prisma.PostGetPayload<{
+	include: typeof postDataInclude
+}>
