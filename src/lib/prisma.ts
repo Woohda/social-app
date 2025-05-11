38c6d9286aaf827ad004f9d4f17aaf207ed32af3
VLAD F <woohda@ya.ru>
2025-05-11 22:48:38 +0800

    fix: add description prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from '@prisma/client'
 import { withAccelerate } from '@prisma/extension-accelerate'
 
+/**
+ * Эта функция создает экземпляр PrismaClient с расширением Accelerate.
+ * Она использует глобальную переменную для хранения экземпляра,
+ * чтобы избежать создания нескольких экземпляров в среде разработки.
+ * В производственной среде создается новый экземпляр.
+ * @property {PrismaClient} prisma - экземпляр PrismaClient
+ * @property {globalForPrisma} globalForPrisma - глобальная переменная для хранения экземпляра PrismaClient
+ */
+
 const globalForPrisma = global as unknown as {
 	prisma: PrismaClient
 }
