50472a7642ba91d20f1d257b352f861a67e3896a
VLAD F <woohda@ya.ru>
2025-05-11 22:47:10 +0800

    fix: add description hook useSession

--- src/hooks/use-session.ts
+++ src/hooks/use-session.ts
@@ -1,6 +1,16 @@
 import { SessionContext } from '@/app/(main)/SessionProvider'
 import { useContext } from 'react'
 
+/**
+ * Пользовательский хук, который предоставляет доступ к контексту сессии.
+ *
+ * Этот хук должен использоваться внутри компонента SessionProvider, так как он зависит от контекста SessionContext.
+ * Если он используется вне компонента SessionProvider, то будет выброшено исключение.
+ *
+ * @returns {SessionContext} Текущий контекст сессии, содержащий информацию о сессии и пользователе.
+ * @throws {Error} Если хук используется вне компонента SessionProvider.
+ */
+
 function useSession() {
 	const context = useContext(SessionContext)
 	if (!context) {
