6ae0dd28fab7f648199900070f3d1d7baa28a7a1
VLAD F <woohda@ya.ru>
2025-05-11 22:46:52 +0800

    fix: add description SessionProvider

--- src/app/(main)/SessionProvider.tsx
+++ src/app/(main)/SessionProvider.tsx
@@ -3,6 +3,16 @@
 import { Session, User } from 'lucia'
 import React, { createContext } from 'react'
 
+/**
+ * эта функция создает контекст для хранения информации о сессии
+ * и пользователе, чтобы избежать передачи их через пропсы
+ * SessionContext - это контекст, который будет хранить информацию о сессии
+ * SessionProvider - это компонент, который будет оборачивать все приложение и передавать информацию о сессии
+ * @param {Session} session - объект сессии
+ * @param {User} user - объект пользователя
+ * @returns {SessionContext} - объект контекста сессии
+ */
+
 interface SessionContext {
 	session: Session | null
 	user: User | null
