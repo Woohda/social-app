3d660d3af8d4c63124b3b7485baf203ff21c6fad
VLAD F <woohda@ya.ru>
2025-06-23 17:15:21 +0700

    feat custom hook use-deletePostMutation

---
+++ src/hooks/use-deletePostMutation.ts
@@ -0,0 +1,68 @@
+import {
+	InfiniteData,
+	useMutation,
+	useQueryClient
+} from '@tanstack/react-query'
+import { useToast } from './use-toast'
+import { deletePost } from '@/features/PostEditor/actions'
+import { PostsPage } from '@/lib/types'
+import { useRouter, usePathname } from 'next/navigation'
+
+/**
+ * Кастомный хук useDeletePostMutation используется для удаления поста на сервере и обновления кэша react-query
+ * при успешном удалении поста
+ * @property {useMutation} - хук useMutation, который используется для отправки запроса на сервер
+ * и обновления кэша react-query
+ * @property {useQueryClient} - хук useQueryClient, который используется для обновления кэша react-query
+ * @property {useToast} - пользовательский хук useToast, который используется для отображения уведомления
+ */
+
+export function useDeletePostMutation() {
+	const { toast } = useToast()
+
+	const queryClient = useQueryClient()
+
+	const router = useRouter()
+	const pathname = usePathname()
+
+	const mutation = useMutation({
+		mutationFn: deletePost,
+		onSuccess: async deletedPost => {
+			const queryFilter = { queryKey: ['post-feed'] }
+
+			await queryClient.cancelQueries(queryFilter)
+
+			queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>(
+				queryFilter,
+				oldData => {
+					if (!oldData) return
+
+					return {
+						pageParams: oldData.pageParams,
+						pages: oldData.pages.map(page => ({
+							posts: page.posts.filter(post => post.id !== deletedPost.id),
+							nextCursor: page.nextCursor
+						}))
+					}
+				}
+			)
+
+			toast({
+				description: 'Пост успешно удален'
+			})
+
+			if (pathname === `/posts/${deletedPost.id}`) {
+				router.push(`/users/${deletedPost.user.username}`)
+			}
+		},
+		onError: error => {
+			console.error(error)
+			toast({
+				description: 'Ошибка: не удалось удалить пост',
+				variant: 'destructive'
+			})
+		}
+	})
+
+	return mutation
+}
