94d44626b735289c05495958c5abd1bc51312ef2
VLAD F <woohda@ya.ru>
2025-06-24 12:29:48 +0700

    fix NavBar add logo

--- src/widgets/NavBar.tsx
+++ src/widgets/NavBar.tsx
@@ -3,7 +3,9 @@
 import SearchField from '@/components/SearchField'
 import dynamic from 'next/dynamic'
 import Link from 'next/link'
+import Image from 'next/image'
 import UserButton from '@/components/UserButton'
+import logo from '@/assets/logo.png'
 
 const NavBar = () => {
 	const ThemeButtonNoSSR = dynamic(() => import('@/components/ThemeButton'), {
@@ -12,8 +14,13 @@ const NavBar = () => {
 	return (
 		<header className='sticky top-0 z-50 w-full border-b bg-card shadow-sm'>
 			<nav className='mx-auto max-w-7xl flex items-center flex-wrap gap-5 py-3 px-5'>
-				<Link href='/' className='text-2xl font-bold text-primary'>
-					Home
+				<Link href='/' className='text-xl font-bold text-primary'>
+					<Image
+						src={logo}
+						alt='Logo'
+						width={60}
+						className='aspect-auto h-fit flex-none object-cover '
+					/>
 				</Link>
 				<SearchField />
 				<UserButton className='sm:ms-auto' />
