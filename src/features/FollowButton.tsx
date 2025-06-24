c8aa5c8d57c62d58c28f083f794328b1353aa739
VLAD F <woohda@ya.ru>
2025-06-24 10:46:19 +0700

    fix use-followerInfo

--- src/features/FollowButton.tsx
+++ src/features/FollowButton.tsx
@@ -3,9 +3,6 @@
 import { FollowerInfo } from '@/lib/types'
 import { Button } from '../components/ui/button'
 import { useFollowerInfo } from '@/hooks/use-followerInfo'
-import kyInstance from '@/lib/ky'
-import { QueryKey, useMutation, useQueryClient } from '@tanstack/react-query'
-import { useToast } from '@/hooks/use-toast'
 
 interface FollowButtonProps {
 	userId: string
@@ -13,42 +10,7 @@ interface FollowButtonProps {
 }
 
 const FollowButton = ({ userId, initialState }: FollowButtonProps) => {
-	const { toast } = useToast()
-
-	const query = useQueryClient()
-	const queryKey: QueryKey = ['follower-info', userId]
-
-	const { data } = useFollowerInfo(userId, initialState)
-
-	const { mutate } = useMutation({
-		mutationFn: () =>
-			data.isFollowedByUser
-				? kyInstance.delete(`api/users/${userId}/followers`)
-				: kyInstance.post(`api/users/${userId}/followers`),
-		onMutate: async () => {
-			await query.cancelQueries({ queryKey })
-			const prevState = query.getQueryData<FollowerInfo>(queryKey)
-			query.setQueryData<FollowerInfo>(queryKey, () => {
-				return {
-					followers:
-						(prevState?.followers || 0) +
-						(prevState?.isFollowedByUser ? -1 : 1),
-					isFollowedByUser: !prevState?.isFollowedByUser
-				}
-			})
-			return { prevState }
-		},
-		onError: (error, variables, context) => {
-			query.setQueryData<FollowerInfo>(queryKey, context?.prevState)
-			toast({
-				description:
-					error instanceof Error
-						? error.message
-						: 'Что-то пошло не так. Попробуйте позже.',
-				variant: 'destructive'
-			})
-		}
-	})
+	const { data, mutate } = useFollowerInfo(userId, initialState)
 
 	return (
 		<Button
--- src/hooks/use-followerInfo.ts
+++ src/hooks/use-followerInfo.ts
@@ -1,22 +1,71 @@
 import kyInstance from '@/lib/ky'
 import { FollowerInfo } from '@/lib/types'
-import { useQuery } from '@tanstack/react-query'
+import {
+	useQuery,
+	useMutation,
+	QueryKey,
+	useQueryClient
+} from '@tanstack/react-query'
+import { useToast } from './use-toast'
 
 /**
- * Кастомный хук useFollowerInfo используется для получения информации о подписчиках пользователя.
- * Он использует react-query для управления состоянием запроса и кэширования данных.
- * @param {string} userId - Идентификатор пользователя, для которого нужно получить информацию о подписчиках.
- * @param {FollowerInfo} initialState - Начальное состояние для запроса, используется при первом рендере.
- * @returns {QueryObserverResult<FollowerInfo>} - Результат запроса, содержащий данные о подписчиках и состояние запроса.
+ * Кастомный хук useFollowerInfo используется для получения информации о подписке пользователя
+ * и обновления кэша react-query при изменении информации о подписке
+ * @property {useQuery} - хук useQuery, который используется для получения информации о подписке пользователя
+ * @property {useMutation} - хук useMutation, который используется для обновления информации о подписке пользователя
+ * и обновления кэша react-query
+ * @property {useToast} - пользовательский хук useToast, который используется для отображения уведомления
+ * @returns {object} Объект с состоянием информации о подписке пользователя и функциями для ее обновления
+ * и обновления кэша react-query
+ * @property {boolean} isFollowedByUser - Признак, указывающий, что пользователь подписан на другого пользователя
+ * @property {number} followers - Количество подписчиков
+ *
  */
 
 export function useFollowerInfo(userId: string, initialState: FollowerInfo) {
-	const query = useQuery<FollowerInfo>({
+	const { toast } = useToast()
+
+	const queryKey: QueryKey = ['follower-info', userId]
+
+	const query = useQueryClient()
+
+	const queryData = useQuery<FollowerInfo>({
 		queryKey: ['follower-info', userId],
 		queryFn: () =>
 			kyInstance.get(`api/users/${userId}/followers`).json<FollowerInfo>(),
 		initialData: initialState,
 		staleTime: Infinity
 	})
-	return query
+
+	const { mutate } = useMutation({
+		mutationFn: () =>
+			queryData.data.isFollowedByUser
+				? kyInstance.delete(`api/users/${userId}/followers`)
+				: kyInstance.post(`api/users/${userId}/followers`),
+		onMutate: async () => {
+			await query.cancelQueries({ queryKey })
+			const prevState = query.getQueryData<FollowerInfo>(queryKey)
+			query.setQueryData<FollowerInfo>(queryKey, () => {
+				return {
+					followers:
+						(prevState?.followers || 0) +
+						(prevState?.isFollowedByUser ? -1 : 1),
+					isFollowedByUser: !prevState?.isFollowedByUser
+				}
+			})
+			return { prevState }
+		},
+		onError: (error, variables, context) => {
+			query.setQueryData<FollowerInfo>(queryKey, context?.prevState)
+			toast({
+				description:
+					error instanceof Error
+						? error.message
+						: 'Что-то пошло не так. Попробуйте позже.',
+				variant: 'destructive'
+			})
+		}
+	})
+
+	return { ...queryData, mutate }
 }
