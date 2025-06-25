5a119b49bbb7d50b2274f42e4352eef86daec55e
VLAD F <woohda@ya.ru>
2025-06-25 22:57:14 +0700

    fix UserProfile changed the component markup

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -20,24 +20,26 @@ const UserProfile = ({ user, loggedInUserId }: UserProfileProps) => {
 		<div className='flex flex-col gap-5 rounded-2xl bg-card p-5 shadow-sm'>
 			<div className='flex flex-wrap items-center gap-5'>
 				<UserAvatar avatarUrl={user.avatarUrl} size={150} />
-				<div>
+				<div className='flex flex-col items-start gap-1'>
 					<h2 className='text-2xl font-bold'>{user.name}</h2>
 					<p className='text-sm text-muted-foreground'>@{user.username}</p>
-					{user.bio && <p className='text-sm mt-3'>user.bio</p>}
-					<p className='text-sm text-muted-foreground'>
-						Аккаунт создан: {formatRelativeDate(user.createdAt)}
+					<p className='text-sm'>
+						<FollowerCount userId={user.id} initialState={followerInfo} />
 					</p>
+					{user.bio && <p className='text-sm mt-3'>user.bio</p>}
 				</div>
 			</div>
 			<div className='w-full flex items-center justify-between'>
-				<div className='flex items-center gap-3'>
+				<div className='flex flex-col items-start'>
+					<p className='text-sm '>
+						Аккаунт создан: {formatRelativeDate(user.createdAt)}
+					</p>
 					<span className='text-sm'>
 						Постов:{' '}
-						<span className='font-semibold text-base'>
+						<span className='font-semibold'>
 							{formatNumber(user._count.posts)}
 						</span>
 					</span>
-					<FollowerCount userId={user.id} initialState={followerInfo} />
 				</div>
 				{user.id === loggedInUserId ? (
 					<span className='text-sm text-muted-foreground'>Это вы</span>
