4c0f127c0eafafb11af01fdd2522688c3a7a22d2
VLAD F <woohda@ya.ru>
2025-06-30 19:02:19 +0700

    fix styles attachmemt preview

--- src/components/AttachmentPreviews.tsx
+++ src/components/AttachmentPreviews.tsx
@@ -12,13 +12,7 @@ const AttachmentsPreviews = ({
 	onRemoveClick
 }: AttachmentsPreviewsProps) => {
 	return (
-		<div
-			className={cn(
-				'flex gap-2',
-				!attachment.length && 'hidden'
-				// attachment.length > 1 && 'sm-grid sm:grid-cols-2'
-			)}
-		>
+		<div className={cn('flex gap-2', !attachment.length && 'hidden')}>
 			{attachment.map(attachment => (
 				<AttachmentsPreview
 					key={attachment.file.name}
