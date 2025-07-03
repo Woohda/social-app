fbe4c9c8847a318d48c8688d7051db6037b6eb52
VLAD F <woohda@ya.ru>
2025-07-03 17:34:34 +0700

    feat userInfoSidebar

--- src/app/(main)/posts/[postId]/page.tsx
+++ src/app/(main)/posts/[postId]/page.tsx
@@ -2,9 +2,11 @@ import { validateRequest } from '@/auth'
 import Post from '@/components/post/Post'
 import prisma from '@/lib/prisma'
 import { getPostDataInclude } from '@/lib/types'
+import UserInfoSidebar from '@/widgets/UserInfoSidebar'
+import { Loader2 } from 'lucide-react'
 import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
-import { cache } from 'react'
+import { cache, Suspense } from 'react'
 
 interface PageProps {
 	params: Promise<{
@@ -47,7 +49,10 @@ export async function generateMetadata({
 			title: `${post.user.name}: ${post.content.slice(0, 50)}...`,
 			description: post.content,
 			url: `/posts/${post.id}`,
-			images: post.attachments ? [post.attachments[0].url] : []
+			images:
+				post.attachments && post.attachments.length > 0
+					? [post.attachments[0].url]
+					: []
 		}
 	}
 }
@@ -69,6 +74,11 @@ export default async function Page({ params: promise }: PageProps) {
 		<main className='w-full min-w-0 flex gap-5'>
 			<div className='w-full flex flex-col gap-5'>
 				<Post post={post} />
+			</div>
+			<div className='sticky top-[5.25rem] hidden h-fit w-72 flex-none space-y-5 md:block lg:w-80'>
+				<Suspense fallback={<Loader2 className='mx-auto animate-spin' />}>
+					<UserInfoSidebar user={post.user} />
+				</Suspense>
 			</div>
 		</main>
 	)
