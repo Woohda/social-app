e399adb583b73ea6fd2f3396fc0bfdcbdbb57025
VLAD F <woohda@ya.ru>
2025-07-04 18:56:23 +0700

    feat add comments on post: add action create and delete comment, CommentButton, CommentInput components

---
+++ src/components/button/CommentButton.tsx
@@ -0,0 +1,21 @@
+import { PostData } from '@/lib/types'
+import { MessageSquareIcon } from 'lucide-react'
+
+interface CommentButtonProps {
+	post: PostData
+	onClick: () => void
+}
+
+const CommentButton = ({ post, onClick }: CommentButtonProps) => {
+	return (
+		<button onClick={onClick} className='flex items-center gap-2'>
+			<MessageSquareIcon className='size-5' />
+			<span className='text-sm font-medium tabular-nums'>
+				{post._count.comments}{' '}
+				<span className='hidden sm:inline'>Комментарии</span>
+			</span>
+		</button>
+	)
+}
+
+export default CommentButton
