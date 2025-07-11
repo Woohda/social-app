cc10a9b84c50768c728653601af9a6a77aec2c2f
VLAD F <woohda@ya.ru>
2025-07-11 19:19:20 +0700

    fix NEXT_PUBLIC_STREAM_API_KEY

--- src/lib/stream.ts
+++ src/lib/stream.ts
@@ -1,7 +1,6 @@
 import { StreamChat } from 'stream-chat'
 
-// для server-side
 export const streamServerClient = StreamChat.getInstance(
-	process.env.STREAM_API_KEY!,
+	process.env.NEXT_PUBLIC_STREAM_API_KEY!,
 	process.env.STREAM_API_SECRET
 )
