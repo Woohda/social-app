3f3ba3008c3c0db5151bc880f65034c1755edef9
VLAD F <woohda@ya.ru>
2025-07-13 18:19:36 +0700

    fix bag prod in hook initialize chat client

--- src/hooks/use-initializeChatClient.ts
+++ src/hooks/use-initializeChatClient.ts
@@ -5,12 +5,10 @@ import kyInstance from '@/lib/ky'
 
 export function useInitializeChatClient() {
 	const { user } = useSession()
-	if (!user) {
-		throw new Error('User is not logged in')
-	}
 	const [chatClient, setChatClient] = useState<StreamChat | null>(null)
 
 	useEffect(() => {
+		if (!user) return
 		const client = StreamChat.getInstance(
 			process.env.NEXT_PUBLIC_STREAM_API_KEY!
 		)
@@ -42,7 +40,7 @@ export function useInitializeChatClient() {
 				})
 				.then(() => console.log('Пользователь успешно отключен от чата'))
 		}
-	}, [user.id, user.username, user.name, user.avatarUrl])
+	}, [user])
 
 	return chatClient
 }
