1c4b19ff2db61c6368d2ed78c8d5f27743eb8c89
VLAD F <woohda@ya.ru>
2025-10-29 22:53:52 +0700

    fix add streamChat component

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,37 +1,37 @@
 'use client'
 
-// import { useInitializeChatClient } from '@/hooks/use-initializeChatClient'
-// import { Loader2 } from 'lucide-react'
-// import { Chat as StreamChat } from 'stream-chat-react'
+import { useInitializeChatClient } from '@/hooks/use-initializeChatClient'
+import { Loader2 } from 'lucide-react'
+import { Chat as StreamChat } from 'stream-chat-react'
 // import ChatSidebar from './ChatSidebar'
 // import ChatChannel from './ChatChannel'
-// import { useTheme } from 'next-themes'
+import { useTheme } from 'next-themes'
 // import { useState } from 'react'
 
 const Chat = () => {
-	// const chatClient = useInitializeChatClient()
-	// const { resolvedTheme } = useTheme()
+	const chatClient = useInitializeChatClient()
+	const { resolvedTheme } = useTheme()
 
 	// const [isOpen, setIsOpen] = useState(false)
 
-	// if (!chatClient) {
-	// 	return <Loader2 className='mx-auto my-3 animate-spin' />
-	// }
+	if (!chatClient) {
+		return <Loader2 className='mx-auto my-3 animate-spin' />
+	}
 
 	return (
 		<main className='relative w-full overflow-hidden rounded-2xl bg-card shadow-sm'>
 			<div className='absolute bottom-0 top-0 flex w-full'>
-				{/* <StreamChat */}
-				{/* client={chatClient}
+				<StreamChat
+					client={chatClient}
 					theme={
 						resolvedTheme === 'dark'
 							? 'str-chat__theme-dark'
 							: 'str-chat__theme-light'
-					} */}
-				{/* > */}
-				{/* <ChatSidebar open={isOpen} onClose={() => setIsOpen(false)} /> */}
-				{/* <ChatChannel open={!isOpen} openSidebar={() => setIsOpen(true)} /> */}
-				{/* </StreamChat> */}
+					}
+				>
+					{/* <ChatSidebar open={isOpen} onClose={() => setIsOpen(false)} /> */}
+					{/* <ChatChannel open={!isOpen} openSidebar={() => setIsOpen(true)} /> */}
+				</StreamChat>
 			</div>
 		</main>
 	)
