498414c144fa313e986acf9763aaa87c6cce6e1f
VLAD F <woohda@ya.ru>
2025-10-29 22:31:34 +0700

    fix hid streamChat component

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,15 +2,15 @@
 
 import { useInitializeChatClient } from '@/hooks/use-initializeChatClient'
 import { Loader2 } from 'lucide-react'
-import { Chat as StreamChat } from 'stream-chat-react'
+// import { Chat as StreamChat } from 'stream-chat-react'
 // import ChatSidebar from './ChatSidebar'
 // import ChatChannel from './ChatChannel'
-import { useTheme } from 'next-themes'
+// import { useTheme } from 'next-themes'
 // import { useState } from 'react'
 
 const Chat = () => {
 	const chatClient = useInitializeChatClient()
-	const { resolvedTheme } = useTheme()
+	// const { resolvedTheme } = useTheme()
 
 	// const [isOpen, setIsOpen] = useState(false)
 
@@ -21,17 +21,17 @@ const Chat = () => {
 	return (
 		<main className='relative w-full overflow-hidden rounded-2xl bg-card shadow-sm'>
 			<div className='absolute bottom-0 top-0 flex w-full'>
-				<StreamChat
-					client={chatClient}
+				{/* <StreamChat */}
+				{/* client={chatClient}
 					theme={
 						resolvedTheme === 'dark'
 							? 'str-chat__theme-dark'
 							: 'str-chat__theme-light'
-					}
-				>
-					{/* <ChatSidebar open={isOpen} onClose={() => setIsOpen(false)} /> */}
-					{/* <ChatChannel open={!isOpen} openSidebar={() => setIsOpen(true)} /> */}
-				</StreamChat>
+					} */}
+				{/* > */}
+				{/* <ChatSidebar open={isOpen} onClose={() => setIsOpen(false)} /> */}
+				{/* <ChatChannel open={!isOpen} openSidebar={() => setIsOpen(true)} /> */}
+				{/* </StreamChat> */}
 			</div>
 		</main>
 	)
