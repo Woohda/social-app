8135efd0890564fb7cee35494ea390a51ce482c5
VLAD F <woohda@ya.ru>
2025-10-30 01:24:19 +0700

    feat search

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,14 @@ const nextConfig: NextConfig = {
 				pathname: '/f/*'
 			}
 		]
+	},
+	rewrites: async () => {
+		return [
+			{
+				source: '/hashtag/:tag',
+				destination: '/search?q=%23:tag'
+			}
+		]
 	}
 }
 
---
+++ src/app/(main)/search/page.tsx
@@ -0,0 +1,34 @@
+import SearchPostsFeed from '@/widgets/feeds/SearchPostsFeed'
+import TrendsSidebar from '@/widgets/TrendsSidebar'
+import { Metadata } from 'next'
+
+interface PageProps {
+	searchParams: { q: string }
+}
+
+// Функция для генерации метаданных страницы поиска по запросу
+// Используется для SEO-оптимизации и улучшения отображения в социальных сетях
+// Возвращает заголовок, описание и Open Graph метаданные для страницы поста.
+export async function generateMetadata({
+	searchParams: { q }
+}: PageProps): Promise<Metadata> {
+	return {
+		title: `Результаты поиска по запросу: "${q}"`
+	}
+}
+
+export default async function Page({ searchParams: { q } }: PageProps) {
+	return (
+		<main className='w-full min-w-0 flex gap-5'>
+			<div className='w-full flex flex-col gap-5'>
+				<div className='rounded-2xl bg-card p-5 shadow-sm'>
+					<h1 className='text-center text-2xl font-bold line-clamp-2 break-all'>
+						Результаты поиска по запросу: &quot;{q}&quot;
+					</h1>
+				</div>
+				<SearchPostsFeed query={q} />
+			</div>
+			<TrendsSidebar />
+		</main>
+	)
+}
